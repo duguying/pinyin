b58871d6381b9995a12c9509e57221424e4ccd57
Rex Lee <duguying2008@gmail.com>
2015-04-27 22:34:26 +0800

    initialize

---
+++ src/main/java/net/duguying/data/README.md
@@ -0,0 +1,5 @@
+## about data ##
+
+here is the data for pinyin-php extension
+
+
---
+++ src/main/java/net/duguying/pinyin/Pinyin.java
@@ -0,0 +1,7 @@
+package net.duguying.pinyin;
+
+/**
+ * Created by rex on 2015/4/27.
+ */
+public class Pinyin {
+}
