953780d6a2d39c5eba5c6afae811930f30e23291
Rex Lee <duguying2008@gmail.com>
2015-09-10 23:17:50 +0800

    parse content into map

--- src/main/java/net/duguying/pinyin/Pinyin.java
+++ src/main/java/net/duguying/pinyin/Pinyin.java
@@ -2,6 +2,8 @@ package net.duguying.pinyin;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by rex on 2015/4/27.
@@ -9,6 +11,8 @@ import java.io.InputStream;
 public class Pinyin {
     private String charsContent = "";
     private String wordsContent = "";
+    private Map<String,String> charsDict = new HashMap<String,String>();
+    private Map<String,String> wordsDict = new HashMap<String,String>();
 
     public Pinyin(){
         try {
@@ -16,14 +20,17 @@ public class Pinyin {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        this.parseChars();
+        this.parseWords();
     }
 
     private void loadContent() throws IOException {
         InputStream isChars = this.getClass().getResourceAsStream("chars.csv");
-        this.charsContent = this.inputStream2String(isChars);
+        this.charsContent = this.inputStream2String(isChars).trim();
 
         InputStream isWords = this.getClass().getResourceAsStream("words.csv");
-        this.wordsContent = this.inputStream2String(isWords);
+        this.wordsContent = this.inputStream2String(isWords).trim();
     }
 
     private String inputStream2String(InputStream in) throws IOException {
@@ -33,5 +40,27 @@ public class Pinyin {
             out.append(new String(b, 0, n));
         }
         return out.toString();
+    }
+
+    private void parseChars(){
+        String[] charsArr = this.charsContent.split("\n");
+        for (String charStr:charsArr){
+            String[] charEle = charStr.split(",");
+            this.charsDict.put(charEle[0].trim(),charEle[1].trim());
+        }
+    }
+
+    private void parseWords(){
+        String[] wordsArr = this.wordsContent.split("\n");
+        for (String wordStr:wordsArr){
+            String[] wordEle = wordStr.split(",");
+            String pinyin = "";
+
+            for (int i = 1; i < wordEle.length; i++){
+                pinyin = pinyin + wordEle[i];
+            }
+
+            this.wordsDict.put(wordEle[0].trim(), pinyin.trim());
+        }
     }
 }
---
+++ src/test/java/net/duguying/pinyin/PinyinTest.java
@@ -0,0 +1,12 @@
+package net.duguying.pinyin;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by duguying on 2015/9/10.
+ */
+public class PinyinTest extends TestCase {
+    public void testPinyin(){
+        Pinyin py = new Pinyin();
+    }
+}
